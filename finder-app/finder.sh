9aa63e1c58e9502c473e19b02926e5d784ded35f
iverimefarishvili <mefarishvili922@gmail.com>
2023-09-27 21:11:12 +0400

    add

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,45 +1,26 @@
 #!/bin/bash
 
-# Default values
-filesdir="/tmp/aeld-data"
-searchstr=""
-
-# Parse command line arguments
-while [[ $# -gt 0 ]]; do
-    key="$1"
-
-    case $key in
-        --filesdir)
-            filesdir="$2"
-            shift
-            shift
-            ;;
-        --searchstr)
-            searchstr="$2"
-            shift
-            shift
-            ;;
-        *)
-            # Unknown option
-            echo "Error: Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
-
-# Check if the search string is specified
-if [ -z "$searchstr" ]; then
-    echo "Error: The search string (--searchstr) is not specified."
-    exit 1
+filesdir=$1
+searchstr=$2
+if [[ $# != 2 ]];
+then
+	echo -e "Enter all arguments"
+	if [ -z "$1" ];
+	then
+		echo -e "Provide file directory"
+	fi
+	if [ -z "$2" ];
+	then
+		echo -e "Provide search string"
+	fi
 fi
+if [ -d $filesdir ];
+then
+	x=$(find $filesdir -type f | wc -l)
+	y=$(grep -hr $searchstr $filesdir | wc -l)
 
-# Check if the files directory exists
-if [ ! -d "$filesdir" ]; then
-    echo "Error: The specified files directory '$filesdir' does not exist."
-    exit 1
+	echo "The number of files are $x and the number of matching lines are $y"
+else
+	echo -e "directory not available"
+	exit 1
 fi
-
-# Search for the search string in files within the directory
-match_count=$(grep -rl "$searchstr" "$filesdir" | wc -l)
-
-echo "The number of files are $(ls "$filesdir" | wc -l) and the number of matching lines are $match_count"
